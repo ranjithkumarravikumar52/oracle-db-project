3a0c9772ef04f7abd750639e2168bf359bd97c79
ranjithkumarravikumar52 <ranjithkumarravikumar52@gmail.com>
2024-07-17 03:59:25 +0530

    populate products

--- sql-scripts/basic_verify_data.sql
+++ sql-scripts/basic_verify_data.sql
@@ -1,16 +1,6 @@
 -- SQL script for verifying data
-
--- Verify Data in Categories
 SELECT * FROM categories;
-
--- Verify Data in Products
 SELECT * FROM products;
-
--- Verify Data in Customers
 SELECT * FROM customers;
-
--- Verify Data in Orders
 SELECT * FROM orders;
-
--- Verify Data in OrderItems
 SELECT * FROM order_items;
